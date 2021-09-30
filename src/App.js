98424c2bc9fd4e775fb6777398650921bbc99ff1
Osama <alhaj2904@gmail.com>
2021-09-30 23:54:09 +0200

    add NavLink

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Nav from "./Components/Nav";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Route  exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <div className="container-about"><h1>About</h1>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dolore laborum voluptas, quas maiores, cum eaque perspiciatis asperiores harum sint magni quidem beatae cumque laboriosam molestias voluptates vero quisquam autem, distinctio ea magnam odio? Ad qui cumque quaerat veritatis et consectetur quis at, vel voluptates provident ipsam assumenda quisquam neque maiores voluptatem? Blanditiis quibusdam iusto, sit voluptas veritatis sed laboriosam voluptate officiis laudantium? Dignissimos quasi blanditiis quisquam officiis vero numquam expedita atque quam quos. Iure nam exercitationem totam a, tenetur unde cumque modi recusandae ex aperiam non error ea doloribus perspiciatis. Dolorum minus laborum fuga labore velit temporibus reiciendis odit.</p></div>
+    </div>
+  );
+}
+
+export default About;
