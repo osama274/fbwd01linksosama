1238e49fbb432392cf8ea19af4096e3971c34a30
Osama <alhaj2904@gmail.com>
2021-10-02 00:22:48 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from "react";
-
-
- import Links from "./data/links.json"
+import { useEffect, useState } from "react";
+import feeds from "./data/feeds.json";
+//  import links from "./data/links.json"
 import "./App.scss";
 
-
 function App() {
-
+  const [links, setLinks] = useState([]);
+  useEffect(() => {
+    feeds.forEach((feed) => {
+      (async () => {
+        const response = await fetch(feed.linksUrl);
+        const tempLinks = await response.json();
+        tempLinks.forEach((tempLink) => (tempLink.origin = feed.name));
+        setLinks((n) => [...n,...tempLinks]);
+      })();
+    });
+  },[]);
   return (
-    
-      <div className="App">
-       <ul>
-         {Links.map((link,index) =>{
-           return(
-             <li key = {index}>{link.title}</li>
-           )
-         })}
-       </ul>
-      </div>
-    
+    <div className="App">
+      <ul>
+        {links.map((link, index) => {
+          return (
+            <li key={index}>
+              <a target="_blank" href={link.url} rel="noreferrer">
+                {link.title}
+              </a>
+              (from {link.origin})
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
