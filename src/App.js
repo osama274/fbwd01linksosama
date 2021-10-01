4768a893d711f8052792a530013d9aa8ec57f45b
Osama <alhaj2904@gmail.com>
2021-10-01 23:34:44 +0200

    display titile

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Nav from "./Components/Nav";
+
+
+ import Links from "./data/links.json"
 import "./App.scss";
 
+
 function App() {
+
   return (
-    <BrowserRouter>
+    
       <div className="App">
-        <Nav />
-        <Route  exact path="/" component={Home} />
-        <Route path="/about" component={About} />
+       <ul>
+         {Links.map((link,index) =>{
+           return(
+             <li key = {index}>{link.title}</li>
+           )
+         })}
+       </ul>
       </div>
-    </BrowserRouter>
+    
   );
 }
 
